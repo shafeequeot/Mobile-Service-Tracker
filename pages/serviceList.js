473b7d1c9b2d394ad83381ab844a1ccb8983bc9b
Shafeeque OT <shafeequeot@gmail.com>
2022-03-12 19:44:57 +0400

    bug fixed

--- pages/serviceList.js
+++ pages/serviceList.js
@@ -66,85 +66,9 @@ ipcRenderer.on('gotID', (key, filters) => {
   })
 })
 
-// ServiceList()
-
-
-function ServiceList() {
-
-  let serviceList = {
-    tableName: commonNames.services,
-    tableData: `*`,
-    where: `id = id`
-
-  }
 
 
 
-  ipcRenderer.invoke("fetchAllDataFromDb", serviceList).then((Data) => {
-
-    // data table work start
 
 
-    let tableEditor = $('#inventoryList').DataTable({
-      dom: "Bfrtip",
-      data: Data,
-      rowId: "id",
-      destroy: true,
-      columns: [
 
-        { data: 'Created_Date' },
-        { data: 'Supplier_Name' },
-        {
-          data: null, render: function (data, type, row) {
-            // Combine the first and last names into a single table field
-            return data.Brand_Name + ' ' + data.Model_No;
-          }
-        },
-
-        { data: 'IMEI' },
-        { data: 'Inv_No' },
-        { data: 'Other' },
-        {
-          data: null,
-          className: "dt-center editor-delete",
-          defaultContent: '<i class="link fa fa-edit"/>',
-          orderable: false
-        }
-      ],
-      select: 'true',
-    })
-
-
-    
-
-
-  })
-
-}
-
-$('#inventoryList').on('click', 'td.editor-delete', function (e) {
-  e.preventDefault();
-
-
-  let dialogMessage = {
-    message: {
-      type: 'error',
-      buttons: ["No", "Edit"],
-      message: "Are your sure to edit?",
-      title: "Are you sure?",
-      cancelId: '0'
-    },
-    quit: false
-  }
-  ipcRenderer.invoke("showMeError", dialogMessage).then((confirmed) => {
-    console.log(confirmed)
-    if (confirmed.response==1) {
-
-     window.location = `./purchase.html?id= ${$(this).closest('tr')[0].id}`
-      // ServiceList()
-
-    }
-  })
-
-
-});
